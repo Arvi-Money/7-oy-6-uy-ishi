2210fa481a3feba070006a3cb7f2195426e31af3
sakiynam <mamatisayevasarvinoz@gmail.com>
2024-03-20 10:38:06 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,72 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isWork, setIsWork] = useState(true); 
+  const [workMinute, setWorkMinute] = useState(25);
+
+  useEffect(() => {
+    if (isActive) {
+      const interval = setInterval(() => {
+        if (second === 0) {
+          if (minute === 0) {
+            setIsWork((prevIsWork) => !prevIsWork);
+            setMinute(isWork ? workMinute : 5); 
+            setSecond(0);
+          } else {
+            setMinute((prevMinute) => prevMinute - 1);
+            setSecond(59);
+          }
+        } else {
+          setSecond((prevSecond) => prevSecond - 1);
+        }
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isActive, isWork, minute, second, workMinute]);
+
+  useEffect(() => {
+    setMinute(isWork ? workMinute : 5);
+    setSecond(0);
+  }, [isWork, workMinute]);
+
+  function timeFormat() {
+    return `${minute < 10 ? '0' + minute : minute}:${second < 10 ? '0' + second : second}`;
+  }
+
+  function handleStart() {
+    setIsActive(true);
+  }
+
+  function handlePause() {
+    setIsActive(false);
+  }
 
   return (
     <>
-     <div className="pomodoro d-flex align-items-center gap-3">
-      <div className='d-flex gap-2 break'>
-        <div>
-        <span>pomodoro</span>
+      <div className="pomodoro d-flex align-items-center gap-3">
+        <div className="d-flex gap-2 break">
+          <div>
+            <span>pomodoro</span>
+          </div>
+          <div>
+            <span>short break</span>
+          </div>
         </div>
         <div>
-        <span>short break</span>
+          <h1>{timeFormat()}</h1> {/* Corrected function call */}
+        </div>
+        <div className="d-flex gap-2">
+          <button onClick={handleStart}>start</button>
+          <button onClick={handlePause}>pause</button>
         </div>
       </div>
-      <div>
-        <h1>25:00</h1>
-      </div>
-      <div className='d-flex gap-2'>
-        <button>start</button>
-        <button>pause</button>
-      </div>
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import addStudentSlice from "./addStudentSlice";
-
-export const store = configureStore({
-    reducer: addStudentSlice,
-})
